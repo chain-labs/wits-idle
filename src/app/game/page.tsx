6dbd1e0afd35e91c03e941728f10855dfbeffe4a
Pratham <chudasamapratham142@gmail.com>
2024-08-31 20:50:00 +0530

    exit modal in new page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -6,8 +6,7 @@ import GameFooter from "@/components/GameFooter";
 import SelectYourNFT from "@/components/SelectYourNFT";
 import { useState } from "react";
 import Modal from "@/components/Modal";
-import { cn } from "@/utils";
-import Link from "next/link";
+import ExitGame from "@/components/modals/ExitGame";
 
 export default function Home() {
   const [openModal, setOpenModal] = useState(false);
@@ -46,42 +45,7 @@ export default function Home() {
 
       {openModal && (
         <Modal>
-          <div className="relative w-full h-full bg-black/75 backdrop-blur-[25px] flex flex-col justify-center items-center z-0">
-            <div
-              onMouseDown={() => setOpenModal(false)}
-              className="absolute inset-0 w-full h-full z-[-1]"
-            ></div>
-            <div
-              className={cn(
-                "rounded-[8px] outline outline-1 outline-offset-4 outline-mediumGold",
-                "bg-gradient-to-tr from-mediumGold to-lightGold",
-                "flex flex-col justify-center items-center",
-                "p-[48px]",
-              )}
-            >
-              <h1 className="text-[32px] text-black uppercase font-medium">
-                ARE YOU SURE
-              </h1>
-              <p className="text-[12px] text-black uppercase font-regular">
-                Do you really want to exit this game?
-              </p>
-
-              <div className="flex justify-center items-center gap-[8px] mt-[16px]">
-                <Link
-                  href="/"
-                  className="bg-black text-lightGold uppercase px-[48px] py-[8px]"
-                >
-                  YES
-                </Link>
-                <button
-                  onClick={() => setOpenModal(false)}
-                  className="bg-black text-lightGold uppercase px-[48px] py-[8px]"
-                >
-                  NO
-                </button>
-              </div>
-            </div>
-          </div>
+          <ExitGame closeModal={() => setOpenModal(false)} />
         </Modal>
       )}
     </div>
